8d06252a5f672e088f6fcf3a2f06a563b65ffdeb
Greyson Mainor <gbmainor98@outlook.com>
2021-03-12 02:30:15 -0500

    added cases and empty functions that will be filled in later.

--- server.js
+++ server.js
@@ -8,15 +8,83 @@ const connection = mysql.createConnection({
     port: 3301,
     user: process.env.DB_USER,
     name: process.env.DB_NAME,
-    password:  process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err
-    console.log("Connected as" +connection.threadId);
+    console.log("Connected as" + connection.threadId);
     prompt();
 });
 
-function prompt(){
+function prompt() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "choice",
+            choices: [
+                "View All Employees?",
+                "View All Employee's By Roles?",
+                "View all Employees By Departments",
+                "Update Employee",
+                "Add Employee?",
+                "Add Role?",
+                "Add Department?"
+            ]
+        }
+    ]).then(function (data) {
+        switch (data.choice) {
+            case "View All Employees?":
+                viewEmployees()
+                break;
+            case "View All Employee's By Roles?":
+                viewRoles()
+                break;
+            case "View all Employees By Departments":
+                viewDepartments()
+                break;
+            case "Update Employee":
+                updateEmployee()
+                break;
+            case "Add Employee?":
+                addEmployee()
+                break;
+            case "Add Role?":
+                addRole()
+                break;
+            case "Add Department?":
+                addDepartment()
+                break;
+        }
+    })
+}
+
+function viewEmployees() {
+
+}
+
+
+function viewRoles() {
+
+}
+
+function viewDepartments() {
+
+}
+
+function updateEmployee() {
+
+}
+
+function addEmployee() {
+
+}
+
+function addRole() {
+
+}
+
+function addDepartment() {
 
 }
