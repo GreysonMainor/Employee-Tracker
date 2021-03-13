2b4d7b48c302304ec405eaa4618b6f5bcfc721ba
Greyson Mainor <gbmainor98@outlook.com>
2021-03-13 14:59:34 -0500

    writing my functions for my index.js file.

--- index.js
+++ index.js
@@ -86,7 +86,35 @@ function addRole() {
 }
 
 function addEmployee() {
-   
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What's the first name of the employee?",
+        name: "fName"
+      },
+      {
+        type: "input",
+        message: "What's the last name of the employee?",
+        name: "lName"
+      },
+      {
+        type: "input",
+        message: "What is the employee's role id number?",
+        name: "roleID"
+      },
+      {
+        type: "input",
+        message: "What is the manager id number?",
+        name: "managerID"
+      }
+    ])
+    .then(function(data) {
+        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [data.fName, data.lName, data.roleID, data.managerID], function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        prompt();
+      });
+    });
 }
 
 function addDepartment() {
